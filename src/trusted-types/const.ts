2d56b3e2423267e04766ea68450eeb544f937123
Krzysztof Kotowicz <koto@google.com>
2019-04-08 15:18:35 +0200

    Const policy works in non-tt supporting browsers.

--- src/trusted-types/const.ts
+++ src/trusted-types/const.ts
@@ -1,11 +1,13 @@
 /// <reference types="trusted-types" />
 
-const ConstPolicy = TrustedTypes.createPolicy('literal-script-url', {
+import { TrustedTypesAvailable } from './default';
+
+const ConstPolicy = TrustedTypesAvailable ? TrustedTypes.createPolicy('literal-script-url', {
     createScriptURL: (s: string) => s,
     createURL: (s: string) => s,
     createScript: (s: string) => s,
     createHTML: (s: string) => s,
-}, false);
+}, false) : null;
 
 
 type LiteralString<S extends string> = string extends S ? 'Not a string literal' : S;
@@ -13,6 +15,11 @@ type LiteralString<S extends string> = string extends S ? 'Not a string literal'
 
 export function scriptUrlFromLiteralString<S extends string>(literalString: LiteralString<S>): TrustedScriptURL&string {
     console.log('You\'re a good programmer and you are loading script from ', literalString);
+    if (!TrustedTypesAvailable) {
+        // For legacy browsers, do nothing. The type system asserts that the function was called with a compile-time
+        // constant, so this is safe.
+        return literalString as unknown as TrustedScriptURL&string;
+    }
     return ConstPolicy.createScriptURL(literalString) as TrustedScriptURL&string;
 }
 
